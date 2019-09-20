a59d62055a5e690b06b95a59c3ffd7a0ca5138d3
DoryDuan <tyhduanjinli@163.com>
2019-09-20 17:57:18 +0800

    hh

--- test/test.js
+++ test/test.js
@@ -1,80 +1,44 @@
-// describe('jQuery', function () {
-//   after(function (done) {
-//     done();
-//   });
-//   it('should have jQuery', function () {
-//     if (!window.jQuery) {
-//       throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
-//     }
-//   })
-//
-//   it('should able to get a body', function () {
-//     var $body = $('body')
-//     $body.length.should.equal(1)
-//     $body[0].should.equal(document.getElementsByTagName('body')[0])
-//   })
-//
-//   describe('should able to trigger an event', function () {
-//     var ele
-//     before(function () {
-//       ele = document.createElement('button')
-//       document.body.appendChild(ele)
-//     })
-//
-//     it('should able trigger an event', function (done) {
-//       $(ele).on('click', function () {
-//         done()
-//       }).trigger('click')
-//     })
-//
-//     after(function () {
-//       document.body.removeChild(ele)
-//       ele = null
-//     })
-//   })
-//
-//   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
-//     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
-//     const url = 'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js';
-//     $.ajax(url, {
-//       success: function () {
-//         done();
-//       }
-//     })
-//   })
-// })
+describe('jQuery', function () {
+  after(function (done) {
+    done();
+  });
+  it('should have jQuery', function () {
+    if (!window.jQuery) {
+      throw new Error('查看下 karma.conf.js 配置项 files 是否正确')
+    }
+  })
 
-var assert = require('assert')
+  it('should able to get a body', function () {
+    var $body = $('body')
+    $body.length.should.equal(1)
+    $body[0].should.equal(document.getElementsByTagName('body')[0])
+  })
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3].indexOf(4)/* 填空题 */)
+  describe('should able to trigger an event', function () {
+    var ele
+    before(function () {
+      ele = document.createElement('button')
+      document.body.appendChild(ele)
+    })
+
+    it('should able trigger an event', function (done) {
+      $(ele).on('click', function () {
+        done()
+      }).trigger('click')
+    })
+
+    after(function () {
+      document.body.removeChild(ele)
+      ele = null
     })
   })
-})
 
-describe('assert', function () {
-  it('a和b应当深度相等', function () {
-    var a = {
-      c: {
-        e: 1
+  it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
+    const url = 'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js';
+    $.ajax(url, {
+      success: function () {
+        done();
       }
-    }
-    var b = {
-      c: {
-        e: 1
-      }
-    }
-    // 修改下面代码使得满足测试描述
-    assert.deepEqual(a, b)
-  })
-
-  it('可以捕获并验证函数fn的错误', function () {
-    function fn() {
-      xxx;
-    }
-    // 修改下面代码使得满足测试描述
-    assert.throws(fn)
+    })
   })
 })
